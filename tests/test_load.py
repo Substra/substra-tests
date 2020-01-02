54a556787150aaf77d923dccb514b216075badca
Jérémy Morel <jeremy.morel@owkin.com>
2020-01-02 17:14:18 +0100

    remove invalid plan

--- tests/test_load.py
+++ tests/test_load.py
@@ -96,58 +96,6 @@ def test_load_multi_node_2_branches(compute_plan_size, global_execution_env):
 
 
 @pytest.mark.parametrize('compute_plan_size', COMPUTE_PLAN_SIZES)
-def test_load_multi_node_aggregates(compute_plan_size, global_execution_env):
-    """
-    Shape of the compute plan:
-
-    Node A: traintuple --> aggregate --> traintuple --> aggregate ...
-    Node B: traintuple /             \-> traintuple /
-    """
-
-    factory, network = global_execution_env
-    session_1 = network.sessions[0].copy()
-    session_2 = network.sessions[1].copy()
-
-    dataset_1 = session_1.state.datasets[0]
-    dataset_2 = session_2.state.datasets[0]
-
-    spec = factory.create_algo()
-    algo = session_1.add_algo(spec)
-
-    spec = factory.create_aggregate_algo()
-    aggregate_algo = session_1.add_aggregate_algo(spec)
-
-    cp_spec = factory.create_compute_plan()
-    previous_aggregatetuple = None
-    for _ in range(compute_plan_size):
-        tuples = [
-            cp_spec.add_traintuple(
-                algo=algo,
-                dataset=dataset,
-                data_samples=[dataset.train_data_sample_keys[0]],
-                in_models=[previous_aggregatetuple] if previous_aggregatetuple else [],
-            ) for dataset in [dataset_1, dataset_2]
-        ]
-        previous_aggregatetuple = cp_spec.add_aggregatetuple(
-            aggregate_algo=aggregate_algo,
-            worker=session_1.node_id,
-            in_models=tuples,
-        )
-
-    cp = session_1.add_compute_plan(cp_spec)
-    first_aggregatetuple = cp.list_aggregatetuple()[0].future().wait()
-    assert first_aggregatetuple.rank == 1
-    assert first_aggregatetuple.status == assets.Status.done
-
-    cp = session_1.cancel_compute_plan(cp.compute_plan_id).future().wait()
-    assert cp.status == assets.Status.canceled
-
-    first_aggregatetuple = cp.list_aggregatetuple()[0]
-    assert first_aggregatetuple.rank == 1
-    assert first_aggregatetuple.status == assets.Status.done
-
-
-@pytest.mark.parametrize('compute_plan_size', COMPUTE_PLAN_SIZES)
 def test_load_multi_node_composite_aggregates(compute_plan_size, global_execution_env):
     """
     Shape of the compute plan:
