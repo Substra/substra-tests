cf7f981b0a0378b4010ff965803dcca029ea1a1f
Jérémy Morel <jeremy.morel@owkin.com>
2020-01-03 17:19:57 +0100

    add manual compute plans

--- tests/test_load.py
+++ tests/test_load.py
@@ -59,6 +59,72 @@ def test_load_linear_traintuples(compute_plan_size, global_execution_env):
 
 
 @pytest.mark.parametrize('compute_plan_size', COMPUTE_PLAN_SIZES)
+def test_load_linear_traintuples_manual(compute_plan_size, global_execution_env):
+    """
+    Shape of the compute plan:
+
+    Node A: traintuple -> traintuple -> ... -> traintuple
+    Node B: traintuple -> traintuple -> ... -> traintuple
+    """
+
+    factory, network = global_execution_env
+    session_1 = network.sessions[0].copy()
+    session_2 = network.sessions[1].copy()
+
+    dataset_1 = session_1.state.datasets[0]
+    dataset_2 = session_2.state.datasets[0]
+
+    spec = factory.create_algo()
+    algo = session_1.add_algo(spec)
+
+    previous_tuples = None
+    compute_plan_id = None
+    for rank in range(compute_plan_size):
+        spec = factory.create_traintuple(
+            algo=algo,
+            dataset=dataset_1,
+            data_samples=[dataset_1.train_data_sample_keys[0]],
+            traintuples=[previous_tuples[0]] if previous_tuples else [],
+            compute_plan_id=compute_plan_id,
+            rank=rank,
+        )
+        tuple_1 = session_1.add_traintuple(spec)
+        # necessary at first step so that we fetch the actual compute plan
+        compute_plan_id = tuple_1.compute_plan_id
+
+        spec = factory.create_traintuple(
+            algo=algo,
+            dataset=dataset_2,
+            data_samples=[dataset_2.train_data_sample_keys[0]],
+            traintuples=[previous_tuples[1]] if previous_tuples else [],
+            compute_plan_id=compute_plan_id,
+            rank=rank,
+        )
+        tuple_2 = session_1.add_traintuple(spec)
+        previous_tuples = [tuple_1, tuple_2]
+
+    # cp = session_1.get_compute_plan(compute_plan_id)
+    # first_tuple_1, first_tuple_2 = cp.list_traintuple()[:2]
+    # assert first_tuple_1.rank == 0
+    # assert first_tuple_2.rank == 0
+    # first_tuple_1 = first_tuple_1.future().wait()
+    # assert first_tuple_1.status == assets.Status.done
+    # first_tuple_2 = first_tuple_2.future().wait()
+    # assert first_tuple_2.status == assets.Status.done
+
+    # cp = session_1.cancel_compute_plan(cp.compute_plan_id).future().wait()
+    # assert cp.status == assets.Status.canceled
+
+    # first_tuple_1, first_tuple_2 = cp.list_traintuple()[:2]
+    # assert first_tuple_1.rank == 0
+    # assert first_tuple_2.rank == 0
+    # first_tuple_1 = first_tuple_1.future().wait()
+    # assert first_tuple_1.status == assets.Status.done
+    # first_tuple_2 = first_tuple_2.future().wait()
+    # assert first_tuple_2.status == assets.Status.done
+
+
+@pytest.mark.parametrize('compute_plan_size', COMPUTE_PLAN_SIZES)
 def test_load_linear_composite_traintuples(compute_plan_size, global_execution_env):
     """
     Shape of the compute plan:
@@ -327,5 +393,87 @@ def test_load_aggregatetuples(compute_plan_size, global_execution_env):
     assert cp.status == assets.Status.canceled
 
     first_aggregatetuple = cp.list_aggregatetuple()[0]
+    assert first_aggregatetuple.rank == 1
+    assert first_aggregatetuple.status == assets.Status.done
+
+
+@pytest.mark.parametrize('compute_plan_size', COMPUTE_PLAN_SIZES)
+def test_load_aggregatetuples_manual(compute_plan_size, global_execution_env):
+    """
+    Shape of the compute plan:
+
+    Node A: composite traintuple --> aggregate --> composite traintuple --> aggregate ...
+    Node B: composite traintuple /             \-> composite traintuple /
+    """
+
+    factory, network = global_execution_env
+    session_1 = network.sessions[0].copy()
+    session_2 = network.sessions[1].copy()
+
+    dataset_1 = session_1.state.datasets[0]
+    dataset_2 = session_2.state.datasets[0]
+
+    spec = factory.create_composite_algo()
+    composite_algo = session_1.add_composite_algo(spec)
+
+    spec = factory.create_aggregate_algo()
+    aggregate_algo = session_1.add_aggregate_algo(spec)
+
+    previous_aggregatetuple = None
+    previous_composite_traintuples = None
+    compute_plan_id = None
+    first_aggregatetuple = None
+    for i in range(compute_plan_size):
+        rank = i*2
+        # composite traintuple 1
+        spec = factory.create_composite_traintuple(
+            algo=composite_algo,
+            dataset=dataset_1,
+            data_samples=[dataset_1.train_data_sample_keys[0]],
+            head_traintuple=previous_composite_traintuples[0] if previous_composite_traintuples else [],
+            trunk_traintuple=previous_aggregatetuple if previous_aggregatetuple else None,
+            rank=rank,
+            compute_plan_id=compute_plan_id,
+        )
+        composite_traintuple_1 = session_1.add_composite_traintuple(spec)
+        compute_plan_id = composite_traintuple_1.compute_plan_id
+
+        # composite traintuple 2
+        spec = factory.create_composite_traintuple(
+            algo=composite_algo,
+            dataset=dataset_2,
+            data_samples=[dataset_2.train_data_sample_keys[0]],
+            head_traintuple=previous_composite_traintuples[1] if previous_composite_traintuples else [],
+            trunk_traintuple=previous_aggregatetuple if previous_aggregatetuple else None,
+            rank=rank,
+            compute_plan_id=compute_plan_id,
+        )
+        composite_traintuple_2 = session_2.add_composite_traintuple(spec)
+
+        previous_composite_traintuples = [
+            composite_traintuple_1,
+            composite_traintuple_2,
+        ]
+
+        spec = factory.create_aggregatetuple(
+            algo=aggregate_algo,
+            worker=session_1.node_id,
+            traintuples=previous_composite_traintuples,
+            rank=rank + 1,
+            compute_plan_id=compute_plan_id,
+        )
+        previous_aggregatetuple = session_1.add_aggregatetuple(spec)
+
+        if not first_aggregatetuple:
+            first_aggregatetuple = previous_aggregatetuple
+
+    first_aggregatetuple = first_aggregatetuple.future().wait()
+    assert first_aggregatetuple.rank == 1
+    assert first_aggregatetuple.status == assets.Status.done
+
+    cp = session_1.cancel_compute_plan(compute_plan_id).future().wait()
+    assert cp.status == assets.Status.canceled
+
+    first_aggregatetuple = first_aggregatetuple.future().wait()
     assert first_aggregatetuple.rank == 1
     assert first_aggregatetuple.status == assets.Status.done
