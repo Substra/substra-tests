ab15917bd8784a649a14a75ae9909b3d4d65f2b2
Jérémy Morel <jeremy.morel@owkin.com>
2020-01-24 10:19:56 +0100

    make load test use all available nodes

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,7 +1,5 @@
 import pytest
 
-from substratest import assets
-
 COMPUTE_PLAN_SIZES = [10, 100, 1000, 10000]
 
 
@@ -10,78 +8,51 @@ def test_load_aggregatetuples_manual(compute_plan_size, global_execution_env):
     """
     Shape of the compute plan:
 
-    Node A: composite traintuple --> aggregate --> composite traintuple --> aggregate ...
-    Node B: composite traintuple /             \-> composite traintuple /
+    Node A  : composite traintuple --> aggregate --> composite traintuple --> aggregate ...
+    Node B-N: composite traintuple /             \-> composite traintuple /
     """
 
     factory, network = global_execution_env
-    session_1 = network.sessions[0].copy()
-    session_2 = network.sessions[1].copy()
-
-    dataset_1 = session_1.state.datasets[0]
-    dataset_2 = session_2.state.datasets[0]
+    sessions = [s.copy() for s in network.sessions]
+    datasets = [s.state.datasets[0] for s in sessions]
 
     spec = factory.create_composite_algo()
-    composite_algo = session_1.add_composite_algo(spec)
+    composite_algo = sessions[0].add_composite_algo(spec)
 
     spec = factory.create_aggregate_algo()
-    aggregate_algo = session_1.add_aggregate_algo(spec)
+    aggregate_algo = sessions[0].add_aggregate_algo(spec)
+
+    spec = factory.create_compute_plan()
+    compute_plan = sessions[0].add_compute_plan(spec)
+    compute_plan_id = compute_plan.compute_plan_id
 
     previous_aggregatetuple = None
     previous_composite_traintuples = None
-    compute_plan_id = None
-    first_aggregatetuple = None
     for i in range(compute_plan_size):
         rank = i*2
-        # composite traintuple 1
-        spec = factory.create_composite_traintuple(
-            algo=composite_algo,
-            dataset=dataset_1,
-            data_samples=[dataset_1.train_data_sample_keys[0]],
-            head_traintuple=previous_composite_traintuples[0] if previous_composite_traintuples else [],
-            trunk_traintuple=previous_aggregatetuple if previous_aggregatetuple else None,
-            rank=rank,
-            compute_plan_id=compute_plan_id,
-        )
-        composite_traintuple_1 = session_1.add_composite_traintuple(spec)
-        compute_plan_id = composite_traintuple_1.compute_plan_id
+        composite_traintuples = []
 
-        # composite traintuple 2
-        spec = factory.create_composite_traintuple(
-            algo=composite_algo,
-            dataset=dataset_2,
-            data_samples=[dataset_2.train_data_sample_keys[0]],
-            head_traintuple=previous_composite_traintuples[1] if previous_composite_traintuples else [],
-            trunk_traintuple=previous_aggregatetuple if previous_aggregatetuple else None,
-            rank=rank,
-            compute_plan_id=compute_plan_id,
-        )
-        composite_traintuple_2 = session_2.add_composite_traintuple(spec)
+        for j in range(len(sessions)):
+            spec = factory.create_composite_traintuple(
+                algo=composite_algo,
+                dataset=datasets[j],
+                data_samples=[datasets[j].train_data_sample_keys[0]],
+                head_traintuple=previous_composite_traintuples[0] if previous_composite_traintuples else [],
+                trunk_traintuple=previous_aggregatetuple if previous_aggregatetuple else None,
+                rank=rank,
+                compute_plan_id=compute_plan_id,
+            )
+            composite_traintuples.append(sessions[j].add_composite_traintuple(spec))
 
-        previous_composite_traintuples = [
-            composite_traintuple_1,
-            composite_traintuple_2,
-        ]
+        previous_composite_traintuples = composite_traintuples
 
         spec = factory.create_aggregatetuple(
             algo=aggregate_algo,
-            worker=session_1.node_id,
+            worker=sessions[0].node_id,
             traintuples=previous_composite_traintuples,
             rank=rank + 1,
             compute_plan_id=compute_plan_id,
         )
-        previous_aggregatetuple = session_1.add_aggregatetuple(spec)
+        previous_aggregatetuple = sessions[0].add_aggregatetuple(spec)
 
-        if not first_aggregatetuple:
-            first_aggregatetuple = previous_aggregatetuple
-
-    first_aggregatetuple = first_aggregatetuple.future().wait()
-    assert first_aggregatetuple.rank == 1
-    assert first_aggregatetuple.status == assets.Status.done
-
-    cp = session_1.cancel_compute_plan(compute_plan_id).future().wait()
-    assert cp.status == assets.Status.canceled
-
-    first_aggregatetuple = first_aggregatetuple.future().wait()
-    assert first_aggregatetuple.rank == 1
-    assert first_aggregatetuple.status == assets.Status.done
+    sessions[0].get_compute_plan(compute_plan_id).future().wait()
