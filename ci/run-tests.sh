c0d80430cc9217d56d4be5ecba7e2967ae556122
Aurélien Gasser <aurelien.gasser@gmail.com>
2020-03-25 17:08:42 -0400

    make it actually work

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -6,15 +6,38 @@ CLUSTER_VERSION="1.15.8-gke.3"
 CLUSTER_ZONE="europe-west4-a"
 CLUSTER_PROJECT="substra-208412"
 SERVICE_ACCOUNT=substra-tests@substra-208412.iam.gserviceaccount.com
-SERVICE_ACCOUNT_KEY="${HOME}/.local/substra-208412-3be0df12d87a.json"
-KANIKO_SERVICE_ACCOUNT_KEY="${HOME}/.local/kaniko-secret.json"
 IMAGE_SUBSTRA_TESTS_DEPLOY_REPO="substrafoundation/substra-tests-deploy"
 IMAGE_SUBSTRA_TESTS_DEPLOY_TAG="latest"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CHARTS_DIR="${DIR}/../charts"
+KEY_SERVICE_ACCOUNT="substra-208412-3be0df12d87a.json"
+KEY_KANIKO_SERVICE_ACCOUNT="kaniko-secret.json"
+
+KEYS_DIR="${HOME}/.local/"
+
+# Parse command-line arguments
+for i in "$@"
+do
+    case $i in
+        -K=*|--keys-directory=*)
+        KEYS_DIR="${i#*=}"
+        shift # past argument=value
+        ;;
+        --default)
+        DEFAULT=YES
+        shift # past argument with no value
+        ;;
+        *)
+            # unknown option
+        ;;
+    esac
+done
+echo "KEYS_DIR  = ${KEYS_DIR}"
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+fi
 
 set -evx
-
 
 # Always delete the cluster, even if the bash script fails
 delete-cluster() {
@@ -25,7 +48,7 @@ trap 'delete-cluster' EXIT
 
 # Log in
 gcloud auth activate-service-account ${SERVICE_ACCOUNT} \
-    --key-file="${SERVICE_ACCOUNT_KEY}"
+    --key-file="${KEYS_DIR}/${KEY_SERVICE_ACCOUNT}"
 
 # Create cluster
 gcloud container clusters create ${CLUSTER_NAME} \
@@ -41,7 +64,7 @@ gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${CLUSTER_ZONE}
 KUBE_CONTEXT=$(kubectl config get-contexts -o name | grep ${CLUSTER_NAME})
 
 # Configure kaniko
-kubectl --context ${KUBE_CONTEXT} create secret generic kaniko-secret --from-file=$KANIKO_SERVICE_ACCOUNT_KEY
+kubectl --context ${KUBE_CONTEXT} create secret generic kaniko-secret --from-file="${KEYS_DIR}/${KEY_KANIKO_SERVICE_ACCOUNT}"
 
 # Configure Tiller
 kubectl --context ${KUBE_CONTEXT} create serviceaccount --namespace kube-system tiller
@@ -67,12 +90,13 @@ helm install ${CHARTS_DIR}/substra-tests-deploy \
 helm install ${CHARTS_DIR}/substra-tests \
     --kube-context ${KUBE_CONTEXT} \
     --name substra-tests \
+    --set image.repository=${REGISTRY}/substrafoundation/substra-tests \
     --set image.tag=local \
     --set deploy.defaultRepo=${REGISTRY}
 
 # Wait for the pod
 SUBSTRA_TESTS_POD=$(kubectl get pods --context ${KUBE_CONTEXT} | grep substra-tests | grep -v kaniko | awk '{print $1}')
-time kubectl wait pod/${SUBSTRA_TESTS_POD} --for=condition=ready --context ${KUBE_CONTEXT} --timeout=900s
+time kubectl wait pod/${SUBSTRA_TESTS_POD} --for=condition=ready --context ${KUBE_CONTEXT} --timeout=1200s
 
 # Run the tests
 kubectl --context ${KUBE_CONTEXT} exec ${SUBSTRA_TESTS_POD} -- make test
