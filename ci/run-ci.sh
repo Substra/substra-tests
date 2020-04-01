6ec873fcdd0dbefde523b889b0b80941d7189aa4
Aurélien Gasser <aurelien.gasser@gmail.com>
2020-04-01 17:33:57 -0400

    force cluster name to start with substra-tests

--- ci/run-ci.sh
+++ ci/run-ci.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
-# This script runs the full substra CI pipeline.
-# "Gotta test them all!" ™
 #
-# It runs the following operations:
-#   - Create a new GKE cluster
-#   - Install a private docker registry on the cluster
-#   - Deploy 'substra-tests-stack', which perfoms the following operations *from within the cluster*:
-#        * Clone the repos:
-#           ~ hlf-k8s
-#           ~ substa-backend
-#           ~ substra-tests
-#           ~ substra
-#        * Build the images to the private registry (kaniko)
-#        * Deploy (helm)
-#   - Wait for the substra application stack to be up and ready (i.e. wait for the substra-backend servers readiness probe)
-#   - Run the full 'substra-tests' test suite
-#   - Destroy the cluster
+# ABOUT
+#   This script runs the full substra CI pipeline.
+#   "Gotta test them all!" ™
 #
-# Useful links:
-# - Travis build logs: https://travis-ci.org/github/SubstraFoundation/substra-tests
-# - Stale cluster deletion script: https://console.cloud.google.com/functions/details/us-central1/clean-substra-tests-ci-deployment
+#   This script performs the following operations:
+#     - Create a new GKE cluster
+#     - Install a private docker registry on the cluster
+#     - Deploy 'substra-tests-stack', which perfoms the following operations
+#       from within the cluster:
+#       - Clone the repos:
+#         - hlf-k8s
+#         - substa-backend
+#         - substra-tests
+#         - substra
+#       - Build the images to the private registry (kaniko)
+#       - Deploy (helm)
+#     - Wait for the substra application stack to be up and ready (i.e. wait
+#       for the substra-backend servers readiness probe)
+#     - Run the full 'substra-tests' test suite
+#     - Destroy the cluster
+#
+# AUTOMATIC CLUSTER DESTRUCTION
+#   We ensure the GKE cluster always gets destroyed at the end of the execution
+#   of this script.
+#
+#   We use 3 mechanisms:
+#   - If no error occurs, we destroy the cluster as the final step
+#   - If an error occurs, or if the script gets interrupted (e.g. Ctrl+C), we
+#     destroy the cluster too (see `trap` command)
+#   - In any other "abrupt shutdown" case (system shutdown, Travis build
+#     canceled, etc), we use a daily scheduled task
+#     to destroy stale clusters after 24h.
+#     See https://console.cloud.google.com/functions/details/us-central1/clean-substra-tests-ci-deployment
+#
+# USEFUL LINKS
+#   Travis build logs: https://travis-ci.org/github/SubstraFoundation/substra-tests
+#   Stale cluster deletion script: https://console.cloud.google.com/functions/details/us-central1/clean-substra-tests-ci-deployment
 
-# Please use a cluster name that starts with 'substra-tests' so that it gets
-# picked up by the automatic stale cluster deletion script.
-CLUSTER_NAME="substra-tests" # overridden with --cluster-name=xyz
+CLUSTER_NAME_ALLOWED_PREFIX="substra-tests"
+CLUSTER_NAME=""
 CLUSTER_MACHINE_TYPE="n1-standard-8"
 CLUSTER_VERSION="1.15.11-gke.1"
 CLUSTER_PROJECT="substra-208412"
@@ -38,32 +54,57 @@ KEYS_DIR="${HOME}/.local/" # overridden with --keys-directory=xyz
 KEY_SERVICE_ACCOUNT="substra-208412-3be0df12d87a.json"
 KEY_KANIKO_SERVICE_ACCOUNT="kaniko-secret.json"
 
+usage() {
+cat <<\EOF
+Usage: run-ci.sh <options>
+  Required option:
+    -N|--cluster-name=<name>        The name if the GKE kubernetes cluster to create
+  Options:
+    -K|--keys-directory=<path>      The path to a folder containing the GKE service account credentials
+    -H|-h|--help                    Display this help"
+EOF
+}
+
 # Parse command-line arguments
 for i in "$@"
 do
     case $i in
-        -K=*|--keys-directory=*)
-        KEYS_DIR="${i#*=}"
-        shift
-        ;;
         -N=*|--cluster-name=*)
         CLUSTER_NAME="${i#*=}"
         shift
         ;;
-        --default)
-        DEFAULT=YES
+        -K=*|--keys-directory=*)
+        KEYS_DIR="${i#*=}"
         shift
+        ;;
+        -H|-h|--help)
+        usage
+        exit 0
         ;;
         *)
             # unknown option
         ;;
     esac
 done
-echo "KEYS_DIR     = ${KEYS_DIR}"
-echo "CLUSTER_NAME = ${CLUSTER_NAME}"
+
 if [[ -n $1 ]]; then
     echo "Last line of file specified as non-opt/last argument:"
 fi
+
+if [[ -z "${CLUSTER_NAME}" ]]; then
+    usage
+    exit 1
+fi
+
+if [[ "${CLUSTER_NAME}" != "${CLUSTER_NAME_ALLOWED_PREFIX}"* ]]; then
+    # The cluster name must start with 'substra-tests'.
+    # This is to ensure the cluster gets picked up by the stale cluster deletion script.
+    echo "ERROR: Invalid cluster name \"${CLUSTER_NAME}\". The cluster name must start with \"${CLUSTER_NAME_ALLOWED_PREFIX}\"."
+    exit 1
+fi
+
+echo "KEYS_DIR     = ${KEYS_DIR}"
+echo "CLUSTER_NAME = ${CLUSTER_NAME}"
 
 set -evx
 
