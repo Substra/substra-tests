24481b7c48942be173e0dd8405d0eaf6631096e8
Aurélien Gasser <aurelien.gasser@owkin.com>
2020-07-27 13:46:10 -0400

    use substratools-minimal (#118)

--- substratest/factory.py
+++ substratest/factory.py
@@ -14,7 +14,7 @@ from . import utils, assets
 
 DEFAULT_DATA_SAMPLE_FILENAME = 'data.csv'
 
-DEFAULT_SUBSTRATOOLS_VERSION = '0.6.0'
+DEFAULT_SUBSTRATOOLS_VERSION = '0.6.0-minimal'
 
 DEFAULT_OPENER_SCRIPT = f"""
 import csv
