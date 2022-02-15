9874f4ce229064a7c0e976628aa29c0b81e08aa6
Samuel Lesuffleur <samlesu@users.noreply.github.com>
2022-02-15 17:02:20 +0100

    feat: cover asset synchronization between orgs (#163)

--- substratest/channel.py
+++ substratest/channel.py
@@ -1,4 +1,5 @@
 import dataclasses
+import json
 import time
 import typing
 
@@ -6,6 +7,26 @@ import substra
 
 from . import cfg
 from . import errors
+
+# fields overriden by the backend that must be stripped in order to anonymize the asset
+_OVERRIDDEN_FIELDS = (
+    # storage address is replaced by each backend with its internal address
+    "storage_address",
+)
+
+
+def _anonymize_asset(asset: dict) -> dict:
+    """Anonymize asset from an organisation point of view."""
+    anonymized_asset = dict(asset)  # make a copy so as not to modify input args
+    for field, value in asset.items():
+
+        if field in _OVERRIDDEN_FIELDS:
+            anonymized_asset[field] = None
+
+        elif isinstance(value, dict):
+            anonymized_asset[field] = _anonymize_asset(value)
+
+    return anonymized_asset
 
 
 @dataclasses.dataclass
@@ -24,29 +45,40 @@ class Channel:
         its own organization.
         """
         unsynchronized_clients = {c.node_id: c for c in self.clients}
+        unsynchronized_assets = {c.node_id: None for c in self.clients}
+
+        reference_asset = _anonymize_asset(asset.dict())
 
         tstart = time.time()
 
         while True:
             if time.time() - tstart > timeout:
+                # provide representation of unsynchronized assets as it will help
+                # to understand quickly a test failure
                 raise errors.SynchronizationTimeoutError(
-                    f"Asset {asset} not synchronized on nodes {','.join(unsynchronized_clients.keys())}"
+                    f"Asset {asset.key} not synchronized: "
+                    f"reference_asset={reference_asset}; "
+                    f"unsynchronized_assets={json.dumps(unsynchronized_assets)}"
                 )
 
-            # iterate on a copy of the dict as we modify its items inside the for loop
+            # iterate on a copy of the dict as its items are modified inside the for loop
             for node_id, client in list(unsynchronized_clients.items()):
                 try:
                     local_asset = client.get(asset)
                 except substra.exceptions.NotFound:
-                    local_asset = None
+                    continue
 
-                if local_asset:
-                    # FIXME to handle asset update we will need to check that the asset
-                    # content is the same from all organisations. This is not
-                    # straightforward as backend of each node is redefining the
-                    # address field for instance.
+                anonymized_asset = _anonymize_asset(local_asset.dict())
+
+                if anonymized_asset == reference_asset:
                     del unsynchronized_clients[node_id]
+                    del unsynchronized_assets[node_id]
+                    continue
 
+                # store the latest state of unsynchronized asset for debugging purposes
+                unsynchronized_assets[node_id] = anonymized_asset
+
+            # all backends have the same representation of the asset, synchronization is done
             if not unsynchronized_clients:
                 return
 
---
+++ tests/test_synchronized.py
@@ -0,0 +1,50 @@
+import pytest
+
+from substratest.factory import AlgoCategory
+
+from . import settings
+
+MSP_IDS = settings.MSP_IDS
+
+
+@pytest.fixture
+def current_client(clients):
+    """Reference client."""
+    return clients[0]
+
+
+@pytest.mark.remote_only
+@pytest.mark.skipif(len(MSP_IDS) < 2, reason="requires at least 2 nodes")
+def test_synchronized_algo_on_creation(factory, channel, current_client):
+    spec = factory.create_algo(AlgoCategory.simple)
+    algo = current_client.add_algo(spec)
+    channel.wait_for_asset_synchronized(algo)
+
+
+@pytest.mark.remote_only
+@pytest.mark.skipif(len(MSP_IDS) < 2, reason="requires at least 2 nodes")
+def test_synchronized_metric_on_creation(factory, channel, current_client):
+    spec = factory.create_metric()
+    metric = current_client.add_metric(spec)
+    channel.wait_for_asset_synchronized(metric)
+
+
+@pytest.mark.remote_only
+@pytest.mark.skipif(len(MSP_IDS) < 2, reason="requires at least 2 nodes")
+def test_synchronized_dataset_on_creation(factory, channel, current_client):
+    spec = factory.create_dataset()
+    dataset = current_client.add_dataset(spec)
+    channel.wait_for_asset_synchronized(dataset)
+
+
+@pytest.mark.remote_only
+@pytest.mark.skipif(len(MSP_IDS) < 2, reason="requires at least 2 nodes")
+def test_synchronized_dataset_on_update(factory, channel, current_client):
+    # create dataset
+    spec = factory.create_dataset()
+    dataset = current_client.add_dataset(spec)
+    channel.wait_for_asset_synchronized(dataset)
+
+    # update dataset by adding a new datasample
+    spec = factory.create_data_sample(datasets=[dataset])
+    current_client.add_data_sample(spec)
