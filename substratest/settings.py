d58855de07060d60710b8380b9712ed74a7ec2fc
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-10-11 14:03:57 +0200

    rebase
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- substratest/settings.py
+++ substratest/settings.py
@@ -12,10 +12,10 @@ _CURRENT_DIR = os.path.dirname(__file__)
 _DEFAULT_NETWORK_CONFIGURATION_PATH = os.path.join(_CURRENT_DIR, "../", "values.yaml")
 _SUBSTRA_TESTS_CONFIG_FILEPATH = os.getenv("SUBSTRA_TESTS_CONFIG_FILEPATH", _DEFAULT_NETWORK_CONFIGURATION_PATH)
 
-_DEFAULT_SUBSTRA_TOOLS_TAG_LOCAL = "class-to-function"  # TODO: change before merge
-#     f"latest-nvidiacuda11.6.0-base-ubuntu20.04-python{sys.version_info.major}.{sys.version_info.minor}"
-# )
-_DEFAULT_SUBSTRA_TOOLS_TAG_REMOTE = "class-to-function"  # "latest" TODO: change before merge
+_DEFAULT_SUBSTRA_TOOLS_TAG_LOCAL = (
+    f"latest-nvidiacuda11.6.0-base-ubuntu20.04-python{sys.version_info.major}.{sys.version_info.minor}"
+)
+_DEFAULT_SUBSTRA_TOOLS_TAG_REMOTE = "latest"
 
 _DEFAULT_SUBSTRA_TOOLS_IMAGE_REMOTE = f"substra/substra-tools:{_DEFAULT_SUBSTRA_TOOLS_TAG_REMOTE}-minimal"
 _DEFAULT_SUBSTRA_TOOLS_IMAGE_LOCAL = f"ghcr.io/substra/substra-tools:{_DEFAULT_SUBSTRA_TOOLS_TAG_LOCAL}-minimal"
