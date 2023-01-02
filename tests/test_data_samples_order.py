7349db1dab548f28b54181d0acaedf66ac2ca169
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-01-02 15:50:15 +0100

    rm test only
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- tests/test_data_samples_order.py
+++ tests/test_data_samples_order.py
@@ -164,11 +164,6 @@ class Dataset:
             spec = factory.create_data_sample(datasets=[dataset])
             client.add_data_sample(spec)
 
-        # create test data samples
-        for _ in range(2):
-            spec = factory.create_data_sample(datasets=[dataset])
-            client.add_data_sample(spec)
-
         self.dataset = client.get_dataset(dataset.key)
         self.train_data_sample_keys = _shuffle(self.dataset.data_sample_keys)
         self.test_data_sample_keys = self.train_data_sample_keys[:2]
