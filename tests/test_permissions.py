a1445b92dc3e1bacb03d5922e060f45a1c546dd6
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-01-19 11:20:47 +0100

    add do not raise
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -1,5 +1,7 @@
 import pytest
 import substra
+from contextlib import contextmanager
+
 
 from substratest.factory import AlgoCategory
 from substratest.factory import AugmentedDataset
@@ -7,6 +9,11 @@ from substratest.factory import Permissions
 from substratest.fl_interface import FLTaskInputGenerator
 from substratest.fl_interface import FLTaskOutputGenerator
 from substratest.fl_interface import OutputIdentifiers
+
+
+@contextmanager
+def does_not_raise():
+    yield
 
 
 @pytest.fixture
@@ -216,14 +223,18 @@ def test_permissions_denied_process(factory, client_1, client_2, channel, worker
 @pytest.mark.remote_only  # no check on permissions with the local backend
 @pytest.mark.slow
 @pytest.mark.parametrize(
-    "client_1_permissions,client_2_permissions,expected_success",
+    "client_1_permissions,client_2_permissions,expectation",
     [
-        (pytest.lazy_fixture("private"), pytest.lazy_fixture("private"), False),
-        (pytest.lazy_fixture("organization_2_only"), pytest.lazy_fixture("private"), True),
+        (
+            pytest.lazy_fixture("private"),
+            pytest.lazy_fixture("private"),
+            pytest.raises(substra.exceptions.AuthorizationError),
+        ),
+        (pytest.lazy_fixture("organization_2_only"), pytest.lazy_fixture("private"), does_not_raise()),
     ],
 )
 def test_permissions_model_process(
-    client_1_permissions, client_2_permissions, expected_success, factory, client_1, client_2, channel, workers
+    client_1_permissions, client_2_permissions, expectation, factory, client_1, client_2, channel, workers
 ):
     """Test that a traintuple can/cannot process an in-model depending on permissions."""
     datasets = []
@@ -271,13 +282,10 @@ def test_permissions_model_process(
         worker=workers[1],
     )
 
-    if expected_success:
+    with expectation:
         traintuple_2 = client_2.add_task(spec)
 
         client_2.wait(traintuple_2)
-    else:
-        with pytest.raises(substra.exceptions.AuthorizationError):
-            traintuple_2 = client_2.add_task(spec)
 
 
 @pytest.mark.remote_only  # no check on permissions with the local backend
@@ -407,10 +415,10 @@ def test_permissions_denied_head_model_process(factory, client_1, client_2, chan
 
 @pytest.mark.remote_only  # no check on permissions with the local backend
 @pytest.mark.parametrize(
-    "permission_train_output, authorize",
+    "permission_train_output, expectation",
     [
-        (pytest.lazy_fixture("organization_1_only"), False),
-        (pytest.lazy_fixture("organization_2_only"), True),
+        (pytest.lazy_fixture("organization_1_only"), pytest.raises(substra.exceptions.AuthorizationError)),
+        (pytest.lazy_fixture("organization_2_only"), does_not_raise()),
     ],
 )
 def test_permission_to_test_on_org_without_training(
@@ -419,15 +427,13 @@ def test_permission_to_test_on_org_without_training(
     organization_2_only,
     client_1,
     client_2,
-    channel,
     factory,
-    authorize,
+    expectation,
 ):
 
     # training algo on client 1
     spec = factory.create_algo(category=AlgoCategory.simple, permissions=organization_1_only)
     train_algo = client_1.add_algo(spec)
-    channel.wait_for_asset_synchronized(train_algo)
 
     # predict and metric algo on client 2
     spec = factory.create_algo(category=AlgoCategory.predict, permissions=organization_2_only)
@@ -468,7 +474,7 @@ def test_permission_to_test_on_org_without_training(
     traintuple_1 = client_1.wait(traintuple_1)
 
     # add testtuple on org 2
-    if authorize:
+    with expectation:
         spec = factory.create_predicttuple(
             algo=predict_algo,
             inputs=dataset_2.test_data_inputs + FLTaskInputGenerator.train_to_predict(traintuple_1.key),
@@ -485,20 +491,4 @@ def test_permission_to_test_on_org_without_training(
         testtuple_2 = client_2.add_task(spec)
         testtuple_2 = client_2.wait(testtuple_2)
 
-    else:
-        with pytest.raises(substra.exceptions.AuthorizationError):
-
-            spec = factory.create_predicttuple(
-                algo=predict_algo,
-                inputs=dataset_2.test_data_inputs + FLTaskInputGenerator.train_to_predict(traintuple_1.key),
-                worker=client_2.organization_id,
-            )
-            predicttuple_2 = client_2.add_task(spec)
-            predicttuple_2 = client_2.wait(predicttuple_2)
-            spec = factory.create_testtuple(
-                algo=metric_algo,
-                inputs=dataset_2.test_data_inputs + FLTaskInputGenerator.predict_to_test(predicttuple_2.key),
-                worker=client_2.organization_id,
-            )
-            testtuple_2 = client_2.add_task(spec)
-            testtuple_2 = client_2.wait(testtuple_2)
+        assert testtuple_2.outputs[OutputIdentifiers.performance].value == pytest.approx(2)
