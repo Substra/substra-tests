a98e5f3d55a2c3a94b36e2ec04a4e0fd203af931
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-04-25 15:43:29 +0200

    fix: test on org without training (#252)
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -480,7 +480,4 @@ def test_permission_to_test_on_org_without_training(
             inputs=dataset_2.test_data_inputs + FLTaskInputGenerator.predict_to_test(predicttask_2.key),
             worker=client_2.organization_id,
         )
-        testtask_2 = client_2.add_task(spec)
-        testtask_2 = client_2.wait(testtask_2)
-
-        assert testtask_2.outputs[OutputIdentifiers.performance].value == pytest.approx(2)
+        client_2.add_task(spec)
